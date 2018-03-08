b84bd204a8bbfffde3d55b46c6cf96881ae91d82
Dajun Tian <tiandajun@gmail.com>
2018-03-07 20:39:23 -0600

    change

--- R/injury_score.R
+++ R/injury_score.R
@@ -10,7 +10,7 @@ injury_score <- function(indata, id_var, dx_var){
   
   #join with ntab_s2
   dx_dict <- ntab_s2
-  
+
   pt_data_w_dxdict <- dplyr::inner_join(cp_indata, dx_dict, by = "dx")
   
   #get maximum score with 9
@@ -24,7 +24,27 @@ injury_score <- function(indata, id_var, dx_var){
                    max_wo_9 = max(severity))
   
   #join together
-  dplyr::full_join(wo_9, w_9, by = c("usubjid", "issbr"))
+  max_w9_wo9 <- dplyr::full_join(wo_9, w_9, by = c("usubjid", "issbr"))
+  
+  #create cross join for template with 0 for each body region
+  iss_template <- merge(dplyr::distinct(cp_indata["usubjid"]), 
+                        dplyr::distinct(dx_dict["issbr"]))
+  iss_template$severity_default <- 0
+  iss_br <- dplyr::full_join(max_w9_wo9, iss_template, by = c("usubjid", "issbr"))
+  
+  # get max score for each body region using the following order
+  # 1-6 - non missing score
+  # 9 - has valid trauma code but with unknow score
+  # 0 - no valid trauma code for that region
+  iss_br <- cbind(iss_br[c("usubjid", "issbr")],
+                  score = dplyr::coalesce(iss_br$max_wo_9, 
+                                          iss_br$max_w_9, 
+                                          iss_br$severity_default))
+  # change body region to be variable name
+  iss_br$issbr <- paste0("br_", iss_br$issbr)
+  
+  # transpose data to be wide
+  iss_br_wide <- tidyr::spread(iss_br, issbr, score)
 }
 
-print((injury_score(pt, patient_id, dx)))
+x<-((injury_score(pt, patient_id, dx)))
