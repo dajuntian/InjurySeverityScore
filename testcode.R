339e560177a7782b1d21acde7321db50126343a0
Dajun Tian <tiandajun@gmail.com>
2018-03-11 23:03:44 -0500

    add testcode

---
+++ testcode.R
@@ -0,0 +1,13 @@
+pat_id <- c(2,2,2,2,2,1,2,1,2,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1)
+icd9 <- c('874.2', '874.8', '900.81', '900.82', '900.89', '805.06', 
+          'E966', '805.07', 'V14.0', '807.02', 'V70.4', '821.01', '823.20', 
+          '860.0', '861.01', '861.21', '861.22', '863.84', '864.04', '865.04', 
+          '865.09', '866.02', '868.04', '958.4')
+sample_data <- data.frame(subj = pat_id, code = icd9, stringsAsFactors = FALSE)
+
+data_wide <- sample_data %>% arrange(subj) %>% 
+  group_by(subj) %>% 
+  mutate(seq = paste0("dx", row_number())) %>% 
+  spread(key = seq, value = code)
+
+
